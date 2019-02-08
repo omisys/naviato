1deb7e508b81603e84c95a2b475899ff1453ed9d
Jop te Dorsthorst <jop.tedorsthorst@zigninnovations.com>
2019-02-08 14:48:48 +0100

    Added recursive folder monitoring

--- src/client.py
+++ src/client.py
@@ -2,8 +2,10 @@
 import os
 import getpass
 import configparser
+from src.watcher import Watcher
 
-class Client():
+
+class Client:
 
     def __init__(self, ip_address, port, uname, dirname):
         self.ip_address = ip_address
@@ -11,10 +13,12 @@ class Client():
         self.uname = uname
         self.dirname = dirname
 
+    @staticmethod
     def read_config():
         print(os.getegid())
 
-    def get_public_key(self):
+    @staticmethod
+    def get_public_key():
         # find public SSH key
         pubkey = None
         pubkey_dirname = os.path.join(get_home_dir(), ".ssh")
@@ -37,10 +41,11 @@ class Client():
 def get_home_dir():
     return os.path.join("/home", getpass.getuser())
 
+
 def read_config():
     # get the absolute path to the config file (can run client.py from anywhere)
     abs_file_path = os.path.abspath(os.path.dirname(__file__))
-    config_path   = os.path.join(abs_file_path, "../config")
+    config_path = os.path.join(abs_file_path, "../config")
 
     # read config file
     config = configparser.RawConfigParser()
@@ -48,14 +53,19 @@ def read_config():
 
     # print some parameters
     seperator = ":"
-    address   = (config.get('example-config', 'ip'), config.get('example-config', 'port'))
+    address = (config.get('example-config', 'ip'), config.get('example-config', 'port'))
     print(seperator.join(address))
+
 
 def main():
     print(getpass.getuser())
     read_config()
-    testClient = Client("192.168.0.1", 1234, getpass.getuser(), None)
-    testClient.get_public_key()
+    testclient = Client("192.168.0.1", 1234, getpass.getuser(), None)
+    # testclient.get_public_key()
+    testclient.dirname = "C:/work"
+    w = Watcher(testclient.dirname)
+    w.run()
+
 
 if __name__ == "__main__":
     main()
---
+++ src/watcher.py
@@ -0,0 +1,46 @@
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+class Watcher:
+
+    def __init__(self, watchdir):
+        self.observer = Observer()
+        self.watchdir = watchdir
+
+    def run(self):
+        event_handler = Handler()
+        self.observer.schedule(event_handler, self.watchdir, recursive=True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(5)
+        except:
+            self.observer.stop()
+            print ("Error")
+
+        self.observer.join()
+
+
+class Handler(FileSystemEventHandler):
+
+    @staticmethod
+    def on_any_event(event):
+
+        if event.event_type == 'created':
+            print("Received created event - %s." % event.src_path)
+
+        elif event.event_type == 'deleted':
+            print("Received deleted event - %s." % event.src_path)
+
+        elif event.event_type == 'modified':
+            print("Received modified event - %s." % event.src_path)
+
+        elif event.event_type == 'moved':
+            print("Received moved event - from %s to %s." % (event.src_path, event.dest_path))
+
+
+if __name__ == '__main__':
+    w = Watcher()
+    w.run()
