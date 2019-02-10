b9c172ecc72698938bfae6627c4d2cba2f94a753
FlyingBBQ <derek.cardol@gmail.com>
2019-02-10 22:12:37 +0100

    server recieve buffer

--- server/server.py
+++ server/server.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 ### server.py ###
 import socket
 
@@ -26,16 +28,56 @@ print("waiting for connection...")
 (clientsocket, address) = serversocket.accept()
 print("Got a connection from %s" % str(address))
 
-filename = ''
 while True:
-    data = clientsocket.recv(1024).decode('utf-8')
-    if not data:
-        break
-    filename += data
+    # receive filename (max 256 bytes)
+    buffer = bytearray(32)
+    view = memoryview(buffer)
 
-print("From connection: " + filename)
-myfile = open(filename, "rb")
-clientsocket.send(myfile.read())
+    received = clientsocket.recv_into(buffer)
+    print(received)
+    filename = view[:received].tobytes().decode('utf-8')
+    #filename = clientsocket.recv(16).decode('utf-8') # Note that you limit your filename length to 255 bytes.
+    if not filename:
+        break
+    print("Filename :" + filename)
+
+    # receive filesize
+    filesize = clientsocket.recv(1024).decode('utf-8')
+    filesize = int(filesize)
+    if not filesize:
+        break
+    print("Filesize :" + filesize)
+
+    f = open(filename, 'wb')
+    if filesize > 0:
+        data = conn.recv(1024)
+        if not data:
+            break
+        # write bytes on file
+        f.write(data)
+
+
+'''
+    size = int(size, 2)
+    filename = clientsocket.recv(size)
+
+    # recieve the filesize as binary data
+    filesize = clientsocket.recv(32)
+    filesize = int(filesize, 2)
+
+    # write the new file
+    file_to_write = open(filename, 'wb')
+    chunksize = 4096
+    while filesize > 0:
+        if filesize < chunksize:
+            chunksize = filesize
+        data = clientsocket.recv(chunksize)
+        file_to_write.write(data)
+        filesize -= len(data)
+
+    file_to_write.close()
+    print("File received successfully")
+    '''
 
 serversocket.close()
 
