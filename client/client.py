939f6b151ca3bf8ce42025109fcbdf113ff21d3e
FlyingBBQ <derek.cardol@gmail.com>
2019-02-11 20:52:56 +0100

    efficient way of sending filesize and filename

--- client/client.py
+++ client/client.py
@@ -6,57 +6,41 @@ import os
 
 CHUNK_SIZE = 1024
 
+directory = "/home/derek/code/naviato"
+filename  = "naviato.png"
+#host     = socket.gethostname()
+#host     = socket.gethostbyname("localhost")
+#host     = '192.168.2.94'
+host      = '127.0.0.1'
+port      = 5037
 
+def bitstring_to_bytes(s):
+    return int(s, 2).to_bytes((len(s) + 7) // 8, byteorder='big')
+
+# set up connection
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#s = socket.socket()
-
-print(socket.gethostname())
-#host = socket.gethostname()
-#host = socket.gethostbyname("localhost")
-host = '192.168.2.94'
-#host = '81.204.229.136'
-port = 5037
-
 s.connect((host, port))
 
-directory = "/home/derek/code/"
-#filename = input("Type shit: ")
-filename = "stm32f411re.pdf"
+# send filename size as 2 bytes
+size = bin(len(filename))[2:].zfill(16)
+s.send(bitstring_to_bytes(size))
 
-'''
-filenamesize = len(filename)
-# encode filename size as 16 bit binary, pad left side with zeros
-filenamesizebin = bin(filenamesize)[2:].zfill(16)
-#print(filenamesizebin)
-filenamesizebytes = bytes(filenamesizebin)
-print(filenamesizebytes)
-
-# send the file size, followed by the filename
-s.send(filenamesizebin.encode('utf-8'))
-'''
+# send filename
 s.send(filename.encode('utf-8'))
 
 filename = os.path.join(directory, filename)
 filesize = os.path.getsize(filename)
-print(filesize)
-s.send(str(filesize).encode('utf-8'))
+print("Filesize:", filesize)
 
+# send filesize as 4 bytes
+size = bin(filesize)[2:].zfill(32)
+s.send(bitstring_to_bytes(size))
+
+'''
 with open(filename, 'rb') as f:
     while filesize > 0:
         data_chunk = f.read(CHUNK_SIZE)
         s.send(data_chunk)
-
-
-'''
-# encode filesize as 32 bit binary
-filesize = bin(filesize)[2:].zfill(32)
-s.send(filesize)
-
-file_to_send = open(filename, 'rb')
-l = file_to_send.read()
-s.sendall(l)
-file_to_send.close()
-print("file sent")
 '''
 
 s.shutdown(socket.SHUT_WR)
--- server/server.py
+++ server/server.py
@@ -5,14 +5,13 @@ import socket
 
 # create socket
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#serversocket = socket.socket()
 
 # set port and hostname visible to outside world
-#host = socket.gethostname()
 print(socket.gethostname())
+#host = socket.gethostname()
 #host = socket.gethostbyname("localhost")
-host = '192.168.2.94'
-#host = '81.204.229.136'
+#host = '192.168.2.94'
+host = '127.0.0.1'
 port = 5037
 
 # bind the socket to port and hostname
@@ -23,61 +22,38 @@ serversocket.listen(5)
 print("waiting for connection...")
 
 # main loop of server
-#while True:
-    # accept connections from outside
-(clientsocket, address) = serversocket.accept()
-print("Got a connection from %s" % str(address))
-
 while True:
-    # receive filename (max 256 bytes)
-    buffer = bytearray(32)
-    view = memoryview(buffer)
+    # accept connections from outside
+    (clientsocket, address) = serversocket.accept()
+    print("Got a connection from %s" % str(address))
 
-    received = clientsocket.recv_into(buffer)
-    print(received)
-    filename = view[:received].tobytes().decode('utf-8')
-    #filename = clientsocket.recv(16).decode('utf-8') # Note that you limit your filename length to 255 bytes.
-    if not filename:
-        break
-    print("Filename :" + filename)
+    while True:
+        # receive filename size as binary data (max 2 bytes)
+        received = clientsocket.recv(2)
+        size = int.from_bytes(received, byteorder='big')
 
-    # receive filesize
-    filesize = clientsocket.recv(1024).decode('utf-8')
-    filesize = int(filesize)
-    if not filesize:
-        break
-    print("Filesize :" + filesize)
-
-    f = open(filename, 'wb')
-    if filesize > 0:
-        data = conn.recv(1024)
-        if not data:
+        # receive filename
+        filename = clientsocket.recv(size).decode('utf-8')
+        if not filename:
             break
-        # write bytes on file
-        f.write(data)
+        print("Filename: " + filename)
 
+        # receive filesize as 4 bytes
+        received = clientsocket.recv(4)
+        filesize = int.from_bytes(received, byteorder='big')
+        if not filesize:
+            break
+        print("Filesize:", filesize)
 
 '''
-    size = int(size, 2)
-    filename = clientsocket.recv(size)
-
-    # recieve the filesize as binary data
-    filesize = clientsocket.recv(32)
-    filesize = int(filesize, 2)
-
-    # write the new file
-    file_to_write = open(filename, 'wb')
-    chunksize = 4096
-    while filesize > 0:
-        if filesize < chunksize:
-            chunksize = filesize
-        data = clientsocket.recv(chunksize)
-        file_to_write.write(data)
-        filesize -= len(data)
-
-    file_to_write.close()
-    print("File received successfully")
-    '''
+        f = open(filename, 'wb')
+        if filesize > 0:
+            data = conn.recv(1024)
+            if not data:
+                break
+            # write bytes on file
+            f.write(data)
+'''
 
 serversocket.close()
 
